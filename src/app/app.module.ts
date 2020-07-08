e7576e95dd0ef7e1688dcebd700ede78fe1e09c9
Matt <matthew9510@gmail.com>
2020-07-07 19:38:46 -0700

    Launch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import { LayoutModule } from "@angular/cdk/layout";
 import { MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./Components/header/header.component";
 import { HomeComponent } from "./Components/home/home.component";
 import { VideosComponent } from "./Components/videos/videos.component";
 import { ShowsComponent } from "./Components/shows/shows.component";
 import { MusicComponent } from "./Components/music/music.component";
 import { ShopComponent } from "./Components/shop/shop.component";
+import { MobileExperienceComponent } from './mobile-experience/mobile-experience.component';
+import { DesktopExperienceComponent } from './desktop-experience/desktop-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     HomeComponent,
     VideosComponent,
     ShowsComponent,
     MusicComponent,
     ShopComponent,
+    MobileExperienceComponent,
+    DesktopExperienceComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mobile-experience/mobile-experience.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-experience',
+  templateUrl: './mobile-experience.component.html',
+  styleUrls: ['./mobile-experience.component.scss']
+})
+export class MobileExperienceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
